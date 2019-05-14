8290b5a9f82412e64ccf0f2d6cfe5d43d5c184ca
Skyllnex <montalto644@gmail.com>
2019-05-14 11:16:11 -0300

    Template Driven Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,16 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 import 'hammerjs';
 import { AppComponent } from './app.component';
@@ -17,14 +27,8 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-
-import { DishService } from './services/dish.service';
-import { PromotionService } from './services/promotion.service';
-import { LeaderService } from './services/leader.service';
-
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { MatDialogModule } from '@angular/material/dialog';
 import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -44,11 +48,15 @@ import { LoginComponent } from './login/login.component';
     MatToolbarModule,
     MatListModule,
     MatGridListModule,
-    FlexLayoutModule,
-    AppRoutingModule,
     MatCardModule,
     MatDialogModule,
-    MatButtonModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [
     DishService,
